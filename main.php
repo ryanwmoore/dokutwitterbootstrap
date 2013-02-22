ea0f98a339195fed66858179e5fb0fe447e1effc
Ryan Moore <rwmoore07@gmail.com>
2013-02-21 23:06:39 -0500

    partial reconstruction -- mix of starter and original broken bootstrap

--- main.php
+++ main.php
@@ -16,7 +16,6 @@ $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER
 <head>
     <meta charset="UTF-8" />
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
-    <?php tpl_metaheaders()?>
     <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
     <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
     <?php tpl_metaheaders() ?>
@@ -28,16 +27,19 @@ $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER
     <link href="<?php echo tpl_getMediaFile(array("css/bootstrap.min.css")); ?>" rel="stylesheet">
     <link href="<?php echo tpl_getMediaFile(array("css/bootstrap-responsive.min.css")); ?>" rel="stylesheet">
 
-
-    <?php tpl_includeFile('meta.html') ?>
-
 </head>
 
 <body data-spy="scroll" data-target="#sidetoc">
+    <?php /* with these Conditional Comments you can better address IE issues in CSS files,
+             precede CSS rules by #IE6 for IE6, #IE7 for IE7 and #IE8 for IE8 (div closes at the bottom) */ ?>
+    <!--[if IE 6 ]><div id="IE6"><![endif]--><!--[if IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
 
+    <?php /* the "dokuwiki__top" id is needed somewhere at the top, because that's where the "back to top" button/link links to */ ?>
+    <?php /* classes mode_<action> are added to make it possible to e.g. style a page differently if it's in edit mode,
+         see http://www.dokuwiki.org/devel:action_modes for a list of action modes */ ?>
+    <?php /* .dokuwiki should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
     <div id="dokuwiki__site"><div id="dokuwiki__top"
         class="dokuwiki site mode_<?php echo $ACT ?>">
-
 
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
@@ -71,77 +73,79 @@ $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER
       </div>
     </div>
 
-    <div class="container-fluid">
-      <div class="row-fluid">
-        <div class="span3">
-          <div class="sidebar-nav affix" id="sidetoc">
 
-            <?php _tpl_toc_to_twitter_bootstrap(); ?>
+        <?php html_msgarea() /* occasional error and info messages on top of the page */ ?>
+        <?php tpl_includeFile('header.html') ?>
 
-          </div><!--/.well -->
-        </div><!--/span-->
-        <div class="span9">
-          <div class="row-fluid">
+        <div class="wrapper">
 
-            <div class="wrapper">
-                <div class="span9" id="dokuwiki__content">
+            <div class="container-fluid">
+            <!-- ********** ASIDE ********** -->
+              <div class="row-fluid">
+                <div class="span3">
+                  <div class="sidebar-nav affix" id="sidetoc">
 
-                    <div class="pad">
+                    <?php _tpl_toc_to_twitter_bootstrap(); ?>
 
-                        <div class="page">
+                  </div><!--/.well -->
+                </div><!--/span-->
+                <div class="span9">
+                  <div class="row-fluid">
 
-                            <?php html_msgarea(); /* occasional error and info messages */ ?>
-                            <?php tpl_flush(); ?>
-                            <?php tpl_content(false); ?>
-                            <div class="clearer"></div>
+                    <div class="wrapper">
+                        <div class="span9" id="dokuwiki__content">
 
+                            <div class="pad">
+
+                                <div class="page">
+
+                                    <?php html_msgarea(); /* occasional error and info messages */ ?>
+                                    <?php tpl_flush(); ?>
+                                    <?php tpl_content(false); ?>
+                                    <div class="clearer"></div>
+
+                                </div>
+
+                            </div>
                         </div>
 
                     </div>
+                  </div><!--/row-->
+                </div><!--/span-->
+              </div><!--/row-->
+            </div><!-- container-fluid -->
+
+            <!-- ********** CONTENT ********** -->
+            <div id="dokuwiki__content"><div class="pad">
+                <?php tpl_flush() /* flush the output buffer */ ?>
+                <?php tpl_includeFile('pageheader.html') ?>
+
+                <div class="page">
+                    <!-- wikipage start -->
+                    <?php tpl_content() /* the main content */ ?>
+                    <!-- wikipage stop -->
+                    <div class="clearer"></div>
                 </div>
 
-            </div>
-          </div><!--/row-->
-        </div><!--/span-->
-      </div><!--/row-->
+                <?php tpl_flush() ?>
+                <?php tpl_includeFile('pagefooter.html') ?>
+            </div></div><!-- /content -->
 
-      <hr>
+            <div class="clearer"></div>
+            <hr class="a11y" />
 
-      <footer class="navbar navbar-static-bottom">
-          <div class="row-fluid">
-            <div class="span12">
+        </div><!-- /wrapper -->
 
-              <div>
-                  <?php _tpl_output_page_tools($showTools, 'li'); ?>
-                  <?php tpl_pageinfo() /* 'Last modified' etc */ ?>
+        <!-- ********** FOOTER ********** -->
+        <div id="dokuwiki__footer"><div class="pad">
+            <div class="doc"><?php tpl_pageinfo() /* 'Last modified' etc */ ?></div>
+            <?php tpl_license('button') /* content license, parameters: img=*badge|button|0, imgonly=*0|1, return=*0|1 */ ?>
+        </div></div><!-- /footer -->
 
-                  <?php tpl_license('button') /* content license, parameters: img=*badge|button|0, imgonly=*0|1, return=*0|1 */ ?>
-                  <?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?>
-                  <?php tpl_includeFile('footer.html') ?>
-              </div>
+        <?php tpl_includeFile('footer.html') ?>
+    </div></div><!-- /site -->
 
-            </div>
-          </div>
-      </footer>
-
-    </div><!--/.fluid-container-->
-
-    <?php
-        //Required by DokuWiki to do its housekeeping (e.g., search indexes).
-        tpl_indexerWebBug();
-    ?>
-
+    <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
     <!--[if ( IE 6 | IE 7 | IE 8 ) ]></div><![endif]-->
-<?php
-                /*
-    //<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.min.js"></script>
-    //<script src="<?php echo tpl_getMediaFile(array("js/bootstrap.min.js")); ?>"></script>
-                    //<script src="<?php echo tpl_getMediaFile(array("js/resolve_jquery_issues.js")); ?>"></script>
-                    //*/
-    ?>
-
-    </div> <!-- dokuwiki__top -->
-    </div> <!-- dokuwiki__site -->
-
 </body>
 </html>
