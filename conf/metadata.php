1befdbe2e422cc07533056606e71f843a8da55ac
Ryan Moore <rwmoore07@gmail.com>
2013-02-22 21:19:01 -0500

    remove deprecated theme settings

--- conf/metadata.php
+++ conf/metadata.php
@@ -7,5 +7,5 @@
 $meta['discussionPage']   = array('string');
 $meta['userPage']         = array('string');
 $meta['hideTools']        = array('onoff');
-$meta['tagline']          = array('string');
-$meta['sidebarID']        = array('string');
+//$meta['tagline']          = array('string');
+//$meta['sidebarID']        = array('string');
